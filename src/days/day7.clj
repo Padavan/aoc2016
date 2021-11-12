01c6fff5abb9a61072b4bc62f8c05567d39c2cef
Dmitrii Shikhaleev <padavankun@gmail.com>
2021-11-12 16:02:41 +0300

    day7part2 added

--- src/days/day7.clj
+++ src/days/day7.clj
@@ -17,7 +17,6 @@
 (defn is-support-tls
   [ip]
   (every? true?
-  ; (println
     (reduce
       (fn [results indexed-word]
         (def idx (first indexed-word))
@@ -38,30 +37,48 @@
 
 ; -- PART 2
 
+(defn is-aba
+  [abc]
+  (and
+    (= (first abc) (last abc))
+    (not= (first abc) (second abc))))
+
 (defn get-sequences
   [word]
-  (println word)
   (loop [index 0 result []]
-    (if (< (-(count word) 2) index)
-      (do (println index))
-      (recur (inc index) result)
-  )
-) 
+    (if (< (-(count word) 3) index)
+      (identity result)
+      (do
+        (def letter-sequence (take 3 (drop index word)))
+        (if (is-aba letter-sequence)
+          (recur (inc index) (conj result letter-sequence))
+          (recur (inc index) result)
+        )
+      )))) 
+
+(defn aba-to-bab
+  [aba]
+  (str/join "" (vector (second aba) (first aba) (second aba)))
+)
+
+(defn word-include-sequence
+  [bab-list word]
+  (some true? (map #(str/includes? word %) bab-list))
+)
 
 (defn is-support-ssl
   [ip]
-  (println ip)
   (def word-collection (str/split ip #"[\[|\]]"))
-  (println "odd: " (take-nth 2 word-collection))
-  (println "2: " (take-nth 2 (rest word-collection)))
+  (def bab-list (map aba-to-bab (mapcat get-sequences (take-nth 2 word-collection))))
+  (if (some true? (map #(word-include-sequence bab-list %) (take-nth 2 (rest word-collection)))) true false)
 )
 
 (defn do-part-2
   "How many IPs in your puzzle input support SSL?"
   [text-list]
-  (is-support-ssl "aba[bab]xyz")
-  ; (println "Part 2: " (count (filter true? (map #(is-support-tls %) text-list ))))
+  (println "Part 2: " (count (filter true? (map #(is-support-ssl %) text-list ))))
 )
+;242
 
 
 (defn runDay7
@@ -69,6 +86,6 @@
   []
   (println "-- Day 7 --")
   (def text-list (getInput "puzzleInput/day7.txt"))
-  ; (do-part-1 text-list)
+  (do-part-1 text-list)
   (do-part-2 text-list)
 )
--- test/days/day7_test.clj
+++ test/days/day7_test.clj
@@ -1,7 +1,7 @@
 (ns days.day7_test
   (:use [clojure.test :refer :all]
         [advent.core :refer :all]
-        [days.day7 :refer [contain-pair is-support-tls]])
+        [days.day7 :refer [contain-pair is-support-tls is-aba is-support-ssl]])
 )
 
 (deftest test-contain-pair
@@ -31,4 +31,24 @@
 
   (testing "Should return true, there are correct pair"
     (is (true? (is-support-tls "ioxxoj[asdfgh]zxcvbn"))))
+)
+
+(deftest test-is-aba
+  (testing "shout return true"
+    (is (true? (is-aba "aba"))))
+
+  (testing "shout return false. letters the same"
+    (is (false? (is-aba "aaa"))))
+    )
+
+(deftest test-is-support-ssl
+  (testing "should reutnr true"
+    (is (true? (is-support-ssl "aba[bab]xyz"))))
+  (testing "should false. xyx is not correspon to xyx"
+    (is (false? (is-support-ssl "xyx[xyx]xyx"))))
+  (testing "should return true. kek eke"
+    (is (true? (is-support-ssl "aaa[kek]eke"))))
+  (testing "should return true. zbz-bzb"
+    (is (true? (is-support-ssl "zazbz[bzb]cdb"))))
+
 )
